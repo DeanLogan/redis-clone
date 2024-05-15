8484917fd47dce73ed9533a6a9350ea494b6319a
Dean Logan <deanlogan42@gmail.com>
2024-05-15 19:34:41 +0100

    Allow for user specified port bindings

--- app/server.go
+++ app/server.go
@@ -1,33 +1,37 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"net"
-	"os"
+    "flag"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379") // port 6379 is the default port for Redis
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
+    port := flag.Int("port", 6379, "port to listen on")
+    flag.Parse()
 
-	fmt.Println("Listening on 6379")
+    l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", *port))
+    if err != nil {
+        fmt.Printf("Failed to bind to port %d\n", *port)
+        os.Exit(1)
+    }
 
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
+    fmt.Printf("Listening on %d\n", *port)
 
-		fmt.Println("Connection accepted")
+    for {
+        conn, err := l.Accept()
+        if err != nil {
+            fmt.Println("Error accepting connection: ", err.Error())
+            os.Exit(1)
+        }
 
-		go handleConnection(conn)
-	}
+        fmt.Println("Connection accepted")
+
+        go handleConnection(conn)
+    }
 }
 
 func handleConnection(conn net.Conn) {
